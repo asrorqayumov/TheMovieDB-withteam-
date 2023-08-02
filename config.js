dded9037dc9220b8e8cd03a872c646df1fb83bf5
Asror Qayumov <qayumovasror2003@gmail.com>
2023-08-02 12:03:43 +0500

    card has displayed

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-
-
-export default  {
-    'BASE_URL':'https://api.themoviedb.org/3',
-    "API_KEY":'05120ae48546745d2c9bc331aeee9751',
-    "TOKEN":'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTEyMGFlNDg1NDY3NDVkMmM5YmMzMzFhZWVlOTc1MSIsInN1YiI6IjY0YmUyM2IwMGVkMmFiMDEzOGY5MTc2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BrsZNttCB7jIOZxzkqXeWwyQPOet1AuqkZ5BzY-Ytfo',
-    'BASE_IMG_URL':'https://image.tmdb.org/t/p/original/',
-    'SESSION_ID':'',
+export default {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: "05120ae48546745d2c9bc331aeee9751",
+  TOKEN:
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTEyMGFlNDg1NDY3NDVkMmM5YmMzMzFhZWVlOTc1MSIsInN1YiI6IjY0YmUyM2IwMGVkMmFiMDEzOGY5MTc2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BrsZNttCB7jIOZxzkqXeWwyQPOet1AuqkZ5BzY-Ytfo",
+  BASE_IMG_URL: "https://image.tmdb.org/t/p/original/",
+  SESSION_ID: "",
 };
