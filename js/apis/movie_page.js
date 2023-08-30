89da0e707f178ee537936cbd6caaec19ab6d35c1
TashtaevUlugbek <tashtaevulugbek@gmail.com>
2023-08-30 11:51:22 +0500

    hell yeah

--- js/apis/movie_page.js
+++ js/apis/movie_page.js
@@ -102,3 +102,22 @@ export async function category(id,type,why) {
     console.log(error);
   }
 }
+export async function states(id){
+  try {
+    const request = await fetch(
+      `${Config.BASE_URL}/movie/872585/account_states`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTEyMGFlNDg1NDY3NDVkMmM5YmMzMzFhZWVlOTc1MSIsInN1YiI6IjY0YmUyM2IwMGVkMmFiMDEzOGY5MTc2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BrsZNttCB7jIOZxzkqXeWwyQPOet1AuqkZ5BzY-Ytfo",
+        },
+      }
+    );
+    let response = await  request.json();
+    return await response;
+  } catch (error) {
+    console.log(error);
+  }
+}
